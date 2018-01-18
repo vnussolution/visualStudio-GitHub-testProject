32b15b91137b2638bd4c136091248d56b27c38b0
VNUS-Solution <vnussolution@gmail.com>
2018-01-17 18:16:31 -0800

    create sum function

--- WebApplication1/WebApplication1/myScript.js
+++ WebApplication1/WebApplication1/myScript.js
@@ -3,3 +3,11 @@ alert(' hello again2222222222');
 alert(' hello again111111111');
 
 console.log(' calling feature 1');
+
+
+let sum = function() {
+    let x1 = 1;
+    let x2 = 2;
+    let result = x1 + x2;
+    return result;
+}
