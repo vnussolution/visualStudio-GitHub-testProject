4b109de8128bd554ce9a6d0f06ac3c360a82245c
VNUS-Solution <vnussolution@gmail.com>
2018-01-17 18:19:18 -0800

    refactor sum function

--- WebApplication1/WebApplication1/myScript.js
+++ WebApplication1/WebApplication1/myScript.js
@@ -5,9 +5,7 @@ alert(' hello again111111111');
 console.log(' calling feature 1');
 
 
-let sum = function() {
-    let x1 = 1;
-    let x2 = 2;
-    let result = x1 + x2;
-    return result;
+let sum = function(x1, x2) {
+
+    return x1 + x2;
 }
